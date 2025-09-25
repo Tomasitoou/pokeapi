8874e127f9624ece28ce029be604d367a377677c
Tomasitoou <tomas.ortizp@uniagustiniana.edu.co>
2025-09-25 21:40:30 +0000

    POKEMON PAG

--- js/pokemon.js
+++ js/pokemon.js
@@ -1,35 +1,25 @@
-async function Pokemon(id){
-        
-        const res = await fetch("https://pokeapi.co/api/v2/pokemon/" + id);
-        const data = await res.json(); 
-
-        console.log(data)
+async function pokemon(id){
+    
         var root = document.getElementById("root");
+        const res = await fetch("https://pokeapi.co/api/v2/pokemon/" + id);
+        const data = await res.json();
 
-        let tipoPoke = "";
-        for (let i = 0; i < data.types.length; i++) {
-            tipoPoke += <span>${data.types[i].type.name}</span> ;
-        }
-
-
-        document.getElementById("uno").classList.add(data.types[0].type.name)
+        let tipoPoke ="";
+        for (let i = 0; i < data.types.length; i++){
+            tipoPoke += `<span>${data.types[i].type.name} </span>`
+    }
+        document.querySelector("body").classList.add (data.types[0].type.name)
         root.innerHTML = `
-        
-        <section class="c-detalle">
+          <section class="c-detalle">
         <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${data.id}.png" alt="${data.name}" height="120" width="auto">
         <p>${data.name}</p>
         <p>${data.id}</p>
-        <p>${tipoPoke}</p>
         <p>Altura: ${data.height / 10} m / Peso: ${data.weight / 10} kg</p>
         <p>hp: ${data.stats[0].base_stat}</p>
         <p>Velocidad: ${data.stats[5].base_stat}</p>
         <p>Ataque: ${data.stats[1].base_stat} Defensa: ${data.stats[2].base_stat}</p>
         <p>Ataque Especial: ${data.stats[3].base_stat} Defensa Especial: ${data.stats[4].base_stat}</p>
-
-    </section>
-        
-        
-        `
+${tipoPoke}
+    </section>`
 }
-
-Pokemon(40)
+pokemon(40)
